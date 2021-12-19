2f4c164804ec3f3726a813c18a979d6a95c5c8fe
Askhat <s.askhat.b@gmail.com>
2021-12-19 23:28:51 +0600

    delete unused files, create Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import { FC, useEffect, useState } from 'react'
+import { Unsplash } from '../../service/Unsplash'
+import { Random } from 'unsplash-js/dist/methods/photos/types';
+import { HeaderProps } from './types'
+import './Header.scss'
+
+const Header: FC<HeaderProps> = () => {
+
+	const [randomPhoto, setRandomPhoto] = useState<Random | any>(null)
+
+	useEffect(() => {
+		Unsplash.photos.getRandom({}).then(({ response }) => {
+			setRandomPhoto(response)
+		}).catch(() => {
+			console.log("something went wrong!");
+		});
+	}, [])
+
+	const bgImage = randomPhoto?.urls?.regular
+
+	return (
+		<div className="Header">
+			<div className="Header__Image">
+				<picture className='Header__Image-Bg'>
+					<source srcSet={bgImage} media="(min-width: 600px)" />
+					<img src={bgImage} alt="MDN" />
+				</picture>
+			</div>
+			<div className='Header__SearchWrapper'>
+				<div className='Header__TitleAndSearchWrapper'>
+					<div className='Header__TitleAndSearchWrapper-Content'>
+						<div className='Header__TitleWrapper'>
+							<h1>Unsplash</h1>
+							<div className='Header__TitleWrapper-Description'>
+								The internet’s source of freely-usable images.<br />
+								Powered by creators everywhere.
+							</div>
+						</div>
+						<div className='Header__Search'>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Header
---
+++ src/service/Unsplash.tsx
@@ -0,0 +1,5 @@
+import { createApi } from "unsplash-js";
+
+export const Unsplash = createApi({
+  accessKey: "5RyFEBkq-f9h9rjRUsspI6jgdxOeu7t6ZrV1F9fLENc"
+});
